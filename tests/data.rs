1fd60e6fa3bc29e18365f7e257da11b51086cc6b
Mykola Humanov <morgenpurple@gmail.com>
2025-04-24 12:54:34 +0300

    tests/data.rs: update

--- tests/data.rs
+++ tests/data.rs
@@ -6,7 +6,7 @@ pub static PARAGRAPH_CONTENTS: &str = r#"
         <div><p role="paragraph">The first paragraph contains <a href="/first" role="link">the first link</a>.</p></div>
         <div><p role="paragraph">The second paragraph contains <a href="/second" role="link">the second link</a>.</p></div>
         <div><p role="paragraph">The third paragraph contains <a href="/third" role="link">the third link</a>.</p></div>
-        <div><p role="paragraph"><mark>highlighted text</mark>, <b>bold text</b></p></div>
+        <div><p id="highlight" role="paragraph"><mark>highlighted text</mark>, <b>bold text</b></p></div>
         <div></div>
     </body>
 </html>"#;
