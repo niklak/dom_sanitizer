80632a2242bbc9b5a189cc35804699883a69161e
Mykola Humanov <morgenpurple@gmail.com>
2025-05-03 12:50:48 +0300

    src/policy/core.rs: revert

--- src/policy/core.rs
+++ src/policy/core.rs
@@ -137,7 +137,7 @@ pub struct AttributeRule<'a> {
 }
 
 #[derive(Debug, Clone)]
-pub struct Policy<'a, T =  Restrictive> {
+pub struct Policy<'a, T: SanitizeDirective =  Restrictive> {
     /// The list of excluding rules for attributes.
     /// For [Permissive] directive: attributes to remove
     /// For [Restrictive] directive: attributes to keep
